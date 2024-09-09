d5f176e06b58c313adc7c8a2b3ef0c484a6c9ef0
Ssionn <casperkizewski@outlook.com>
2024-09-09 13:49:15 +0200

    Added route for homepage

--- game_of_life_project/urls.py
+++ game_of_life_project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from game import views
 
 urlpatterns = [
+    path('home/', views.home_page, name='home'),
+
     path('admin/', admin.site.urls),
 ]
+
